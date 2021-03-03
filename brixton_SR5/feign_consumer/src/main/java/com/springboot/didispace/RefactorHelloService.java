1c59dee052e307fcb3a81ca7fe0c1f2e1dcbccbe
liyuan3210 <li.yuan3210@163.com>
2021-03-03 19:25:05 +0800

    tj

--- brixton_SR5/feign_consumer/src/main/java/com/springboot/didispace/RefactorHelloService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.didispace;
-
-import org.springframework.cloud.netflix.feign.FeignClient;
-//import com.springboot.didispace.service.HelloService;
-//
-//@FeignClient(value="HELLO-SERVICE")
-//public interface RefactorHelloService extends HelloService {
-//
-//}
